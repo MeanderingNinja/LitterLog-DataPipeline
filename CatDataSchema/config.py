bef2b37e15e36427116ee7b5a196e881254fff8d
cat_dev <emma.lijing0723@gmail.com>
2022-08-26 17:32:11 -0700

    new file:   CatDataSchema/alembic/versions/9722539bc713_initiate_model_20220823.py 	modified:   CatDataSchema/config.py

--- CatDataSchema/config.py
+++ CatDataSchema/config.py
@@ -4,5 +4,5 @@ import os
 # The syntax: dialect+driver://postgres_username:password@host:port/database
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+psycopg2://emma_dev:emma_dev@192.168.1.157:5432/cat_tech_database",
+    "postgresql+psycopg2://metabase_catwatcher_user:metabase_catwatcher_pw@192.168.1.157:5432/metabase_catwatcher_db",
 )
