3f635c6a8107286b2cdc4472b3148f433d2591ab
cat_dev <emma.lijing0723@gmail.com>
2023-06-06 14:29:48 -0700

    modified:   CatDataSchema/alembic/env.py

--- CatDataSchema/alembic/env.py
+++ CatDataSchema/alembic/env.py
@@ -24,7 +24,8 @@ if config.config_file_name is not None:
 from CatDataSchema.models import (
     Base,
     CatData,
-)  
+)
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -71,9 +72,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
